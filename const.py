17134de779076628e21576c700a5fc2bd859e0bd
kyoky-Fundasta <kyokyoku-an@fundasta.co.jp>
2024-07-17 22:19:00 +0900

    pinecone env

--- const.py
+++ const.py
@@ -19,6 +19,7 @@ env_openai = st.secrets["OPENAI_API_KEY_FundastA"]
 env_genai = st.secrets["Gemini_API_KEY_An"]
 env_smith = st.secrets["LANGCHAIN_API_KEY"]
 env_pinecone = st.secrets["Pinecone_API_KEY"]
+pinecone_environment = st.secrets["PINECONE_ENVIRONMENT"]
 
 
 # GraphState is a TypedDict that defines the structure of the  graph state
