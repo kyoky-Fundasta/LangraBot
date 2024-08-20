b97d5fffb580ee00b562a3ec213d2527087474b9
kyoky-Fundasta <kyokyoku-an@fundasta.co.jp>
2024-08-20 13:52:35 +0900

    Cognito code added to the main

--- module/llm/BasicTripletChains.py
+++ module/llm/BasicTripletChains.py
@@ -1,6 +1,7 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.output_parsers import StrOutputParser
 from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
 
 from data.const import (
     env_genai,
